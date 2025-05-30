562d705a28d54fb73dd0df78708950896daf96d9
Hirayama_Evan <hirae675@hsd.k12.or.us>
2025-05-30 10:45:21 -0700

    Adding player movement constraints

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 3, -4);
+    private Vector3 offset = new Vector3(0, 3, -7);
     private float turnSpeed = 50.0f;
     private float horizontalInput;
 
@@ -16,8 +16,6 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        
-
         transform.position = player.transform.position + offset;
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,8 @@ public class PlayerMovement : MonoBehaviour
     private float speed = 15.0f;
     private float horizontalInput;
     private float verticalInput;
-    
+    private float sideBounds = 23;
+    private float turnSpeed = 100.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,6 +23,23 @@ public class PlayerMovement : MonoBehaviour
 
         //Player movement
         transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
-        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+
+        //player rotation
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    
+        if (transform.position.x < -sideBounds)
+        {
+            transform.position = new Vector3(-sideBounds, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > sideBounds)
+        {
+            transform.position = new Vector3(sideBounds, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.z < -sideBounds)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -sideBounds);
+        }
     }
 }
